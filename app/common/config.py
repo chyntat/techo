8b78ebeff0a355e40462f7b0acb4b58c374273b9
SaketKr <saket@proshort.ai>
2023-02-11 14:48:47 +0530

    connected homepage with api

--- app/common/config.py
+++ app/common/config.py
@@ -5,11 +5,11 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str = 'postgresql+asyncpg://postgres:postgres123@127.0.0.1:54320/techo'
-    base_url: str = 'http://localhost:5432'
+    base_url: str = 'http://localhost:8000'
     local_env: bool = True
 
-    # class Config:
-    #     env_file = os.getenv('ENV_FILE', 'local.env')
+    class Config:
+        env_file = os.getenv('ENV_FILE', 'dev.env')
 
 
 @lru_cache()
